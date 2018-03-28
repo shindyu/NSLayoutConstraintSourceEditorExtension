d92ced7208b86e7e4b7661c6d788f37014bc7945
shindyu <shindyu.dev@gmail.com>
2018-03-28 22:12:19 +0900

    Initial Commit

---
+++ SourceEditorExtension/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SourceEditorExtension
+//
+//  Created by 新堂　敬隆 on 2018/03/28.
+//  Copyright © 2018年 shindyu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
